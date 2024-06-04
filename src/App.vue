bd1884e79a2ebc12caf3c6cfe2b055671d1c6167
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-06-04 19:52:30 -0300

    first commit, add css resets, variable colors and install fonts

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Hola mundis</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis animi ex sapiente ea magni sequi earum autem ipsa cupiditate vel impedit aspernatur voluptatem, necessitatibus dolorum beatae aperiam sunt atque? Qui!</p>
+</template>
+
+<style scoped>
+  h1 {
+    font-family: var(--display-font);
+  }
+
+  p{ font-family: var(--default-font);}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import '@fontsource/carter-one';
+import '@fontsource/readex-pro';
+import './style.css'
+import App from './App.vue'
+
+
+createApp(App).mount('#app')

70a1b389a0633cd792b5557a3d7bcf6f5308ab57
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-06-05 11:40:30 -0300

    Add boxicons dependency and a side-navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,67 @@
 <script setup>
+  import Navbar from './components/Navbar.vue';
+
 </script>
 
 <template>
-  <h1>Hola mundis</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis animi ex sapiente ea magni sequi earum autem ipsa cupiditate vel impedit aspernatur voluptatem, necessitatibus dolorum beatae aperiam sunt atque? Qui!</p>
+  <section class="principal-container">
+    <header class="principal-container__header">
+      <Navbar />
+    </header>
+    <main class="principal-container__content">
+      <h1>Main title</h1>
+    </main>
+  </section>
 </template>
 
 <style scoped>
-  h1 {
-    font-family: var(--display-font);
-  }
 
-  p{ font-family: var(--default-font);}
+.principal-container{
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: minmax(50px, 250px) 1fr;
+  justify-items: start;
+}
+
+.principal-container > * {
+  height: 100vh;
+}
+
+.principal-container__header{
+  position: relative;
+  width: min-content;
+  background-color: var(--beige);
+  z-index: 1;
+}
+
+
+.principal-container__header:has(.navbar_open){
+  border-radius: 0 25px 25px 0;
+  animation: myAnim .3s ease 0s 1 normal forwards;
+}
+
+.principal-container__header:has(.navbar_open) + .principal-container__content{
+  background-color: var(--modal-fade-bg);
+  pointer-events: none;
+}
+
+
+.principal-container__content{
+  margin-left: 50px;
+  position: absolute;
+  width: calc(100% - 50px);
+}
+
+@keyframes myAnim {
+	0% {
+		opacity: 0;
+		transform: translateX(-250px);
+	}
+
+	100% {
+		opacity: 1;
+		transform: translateX(0);
+	}
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import '@fontsource/carter-one';
 import '@fontsource/readex-pro';
+import 'boxicons';
 import './style.css'
 import App from './App.vue'
 
